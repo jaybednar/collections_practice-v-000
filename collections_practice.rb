25b4cca8a804767e93b47bda02a2c3337a7f5821
Jay Bednar <ironviralmedia@gmail.com>
2018-02-07 23:00:09 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+
+def sort_array_desc(array)
+  array.sort do |a,b|
+    b <=> a
+  end
+end
+
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+
+def swap_elements(array)
+  pos_placeholder = array[1]
+  array[1] = array[2]
+  array[2] = pos_placeholder
+  return array
+end
+
+
+def swap_elements_from_to(array, index, destination_index)
+  pos_placeholder = array[index]
+  array[index] = array[destination_index]
+  array[destination_index] = pos_placeholder
+  return array
+end
+
+
+def reverse_array(array) 
+  array.sort.reverse
+end 
